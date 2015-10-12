0dbd21fc84589b7cf77ed172c1eaeacd74017dfe
Robert Messerle <rmesserle@google.com>
2015-10-12 12:04:04 -0700

    removes docs folder from 0.11.3

--- 0.11.3/docs/demo-partials/select/demoValidations/script.js
+++
@@ -1,9 +0,0 @@
-angular.module('selectDemoValidation', ['ngMaterial', 'ngMessages'])
-.controller('AppCtrl', function($scope) {
-  $scope.clearValue = function() {
-    $scope.myModel = undefined;
-  };
-  $scope.save = function() {
-    alert('Form was valid!');
-  };
-});
--- 0.11.3/docs/js/build-config.js
+++
@@ -1,7 +0,0 @@
-DocsApp.constant('BUILDCONFIG', {
-  "ngVersion": "1.4.6",
-  "version": "0.11.3",
-  "repository": "https://github.com/angular/material",
-  "commit": "708422c0fdc497f4d7d81d1701a1b23f082f6548",
-  "date": "2015-10-12 11:11:22 -0700"
-});
