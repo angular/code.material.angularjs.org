50eddf2b3e7b1095d3b178fd0f77fd2466e3ae62
Robert Messerle <rmesserle@google.com>
2015-10-18 11:45:50 -0700

    release: version 0.12.0-rc1

---
+++ 0.12.0-rc1/demo-partials/toolbar/demoScrollShrink/script.js
@@ -0,0 +1,19 @@
+var app = angular.module('toolbarDemo2', ['ngMaterial']);
+
+app.controller('TitleController', function($scope) {
+  $scope.title = 'My App Title';
+});
+
+app.controller('AppCtrl', function($scope) {
+  var imagePath = 'img/list/60.jpeg';
+
+  $scope.todos = [];
+  for (var i = 0; i < 15; i++) {
+    $scope.todos.push({
+      face: imagePath,
+      what: "Brunch this weekend?",
+      who: "Min Li Chan",
+      notes: "I'll be in your neighborhood doing errands."
+    });
+  }
+});
---
+++ 0.12.0-rc1/js/build-config.js
@@ -0,0 +1,7 @@
+DocsApp.constant('BUILDCONFIG', {
+  "ngVersion": "1.4.6",
+  "version": "0.12.0-rc1",
+  "repository": "https://github.com/angular/material",
+  "commit": "a400e362e9b2d698f542d8cb940278d99d32a7e5",
+  "date": "2015-10-18 11:45:05 -0700"
+});
