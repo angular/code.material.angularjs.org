2169d676af9e53d490caaf5520cf02396249b8f3
Robert Messerle <rmesserle@google.com>
2015-10-29 16:38:16 -0700

    release: version 1.0.0-rc2

--- 1.0.0-rc1/js/build-config.js
+++
@@ -1,7 +0,0 @@
-DocsApp.constant('BUILDCONFIG', {
-  "ngVersion": "1.4.6",
-  "version": "1.0.0-rc1",
-  "repository": "https://github.com/angular/material",
-  "commit": "49e11ca338fd1ce2026927a2a93269f3ee7b6509",
-  "date": "2015-10-21 09:38:54 +0100"
-});
