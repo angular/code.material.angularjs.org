df74805cc41de8ce55a1820cf4af81a1aacec2e7
Jeremy Elbourn <jelbourn@google.com>
2016-01-28 15:22:49 -0800

    release: version 1.0.4

---
+++ 1.0.4/js/highlight-angular.js
@@ -0,0 +1,68 @@
+DocsApp
+
+.directive('hljs', ['$timeout', '$q', '$interpolate', function($timeout, $q, $interpolate) {
+  return {
+    restrict: 'E',
+    compile: function(element, attr) {
+      var code;
+      //No attribute? code is the content
+      if (!attr.code) {
+        code = element.html();
+        element.empty();
+      }
+
+      return function(scope, element, attr) {
+
+        if (attr.code) {
+          // Attribute? code is the evaluation
+          code = scope.$eval(attr.code);
+        }
+        var shouldInterpolate = scope.$eval(attr.shouldInterpolate);
+
+        $q.when(code).then(function(code) {
+          if (code) {
+            if (shouldInterpolate) {
+              code = $interpolate(code)(scope);
+            }
+            var contentParent = angular.element(
+              '<pre><code class="highlight" ng-non-bindable></code></pre>'
+            );
+            element.append(contentParent);
+            // Defer highlighting 1-frame to prevent GA interference...
+            $timeout(function() {
+              render(code, contentParent);
+            }, 34, false);
+          }
+        });
+
+        function render(contents, parent) {
+
+          var codeElement = parent.find('code');
+          var lines = contents.split('\n');
+
+          // Remove empty lines
+          lines = lines.filter(function(line) {
+            return line.trim().length;
+          });
+
+          // Make it so each line starts at 0 whitespace
+          var firstLineWhitespace = lines[0].match(/^\s*/)[0];
+          var startingWhitespaceRegex = new RegExp('^' + firstLineWhitespace);
+          lines = lines.map(function(line) {
+            return line
+              .replace(startingWhitespaceRegex, '')
+              .replace(/\s+$/, '');
+          });
+
+          var highlightedCode = hljs.highlight(attr.language || attr.lang, lines.join('\n'), true);
+          highlightedCode.value = highlightedCode.value
+            .replace(/=<span class="hljs-value">""<\/span>/gi, '')
+            .replace('<head>', '')
+            .replace('<head/>', '');
+          codeElement.append(highlightedCode.value).addClass('highlight');
+        }
+      };
+    }
+  };
+}])
+;
