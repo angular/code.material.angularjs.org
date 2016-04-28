7c2f70d6700ebed3ad1535a96579b057619f75a9
Jeremy Elbourn <jelbourn@google.com>
2016-04-28 13:56:01 -0700

    release: version 1.0.8

--- 1.1.0-rc4/demo-partials/slider/demoVertical/script.js
+++
@@ -1,9 +0,0 @@
-
-angular.module('sliderDemo2', ['ngMaterial'])
-
-.controller('AppCtrl', function($scope) {
-
-  $scope.vol = Math.floor(Math.random() * 100);
-  $scope.bass = Math.floor(Math.random() * 100);
-  $scope.master = Math.floor(Math.random() * 100);
-});
--- 1.1.0-rc4/js/build-config.js
+++
@@ -1,7 +0,0 @@
-DocsApp.constant('BUILDCONFIG', {
-  "ngVersion": "1.4.8",
-  "version": "1.1.0-rc4",
-  "repository": "https://github.com/angular/material",
-  "commit": "1dbf1777abc120128f1756c297830298296292a3",
-  "date": "2016-04-15 14:24:06 -0700"
-});
