8cb23b3cb8b0d63c39ff912847da15caa4aa5911
Thomas Burleson <ThomasBurleson@Gmail.com>
2017-06-14 23:16:20 +0000

    snapshot: 1.1.4-master-aa92290

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.5.5",
   "version": "1.1.4",
   "repository": "https://github.com/angular/material",
-  "commit": "8eb8cc7b1e506e990b6bfa331d93eda4a69d5b53",
-  "date": "2017-06-14 17:04:10 -0500"
+  "date": "2017-06-14 17:56:35 -0500",
+  "commit": "aa922904e642f21d7c445303ee43040fe232aeea"
 });
