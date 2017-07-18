f8d495d167569163c3b8daad4d775838c357838d
João Amaral Craveiro <joao@amaralcraveiro.com>
2017-07-18 20:00:39 +0000

    snapshot: 1.1.4-master-9430a7c

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.5.5",
   "version": "1.1.4",
   "repository": "https://github.com/angular/material",
-  "date": "2017-07-18 12:48:32 -0700",
-  "commit": "28d4bf2cb6cb23416adbe182d282eba42e14ae93"
+  "date": "2017-07-18 12:49:12 -0700",
+  "commit": "9430a7cbdcf083f3d3492c1928236fcedbbd9167"
 });
