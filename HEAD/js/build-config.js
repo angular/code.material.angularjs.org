23c539003152d3c24a81d2c30f4c5e546b2ab3de
Jessica Yoshimi <jeyoshimi@users.noreply.github.com>
2017-07-18 20:00:10 +0000

    snapshot: 1.1.4-master-28d4bf2

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.5.5",
   "version": "1.1.4",
   "repository": "https://github.com/angular/material",
-  "date": "2017-07-18 11:14:37 -0700",
-  "commit": "17f09dcc5d2093a54e34b2bba73ac8d2237f8413"
+  "date": "2017-07-18 12:48:32 -0700",
+  "commit": "28d4bf2cb6cb23416adbe182d282eba42e14ae93"
 });
