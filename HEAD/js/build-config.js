34f934a7d2c23c0a4a3b87d12839bbe8bf1d679b
Piotr Karny <public.veritas@gmail.com>
2017-07-18 17:03:26 +0000

    snapshot: 1.1.4-master-c1b715f

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.5.5",
   "version": "1.1.4",
   "repository": "https://github.com/angular/material",
-  "date": "2017-07-18 09:45:23 -0700",
-  "commit": "0151b4bed9d90aa942734591615878f5fcc0fe52"
+  "date": "2017-07-18 09:52:18 -0700",
+  "commit": "c1b715f0094a4e6b7c1b49ecc94ec54ab441e97a"
 });
