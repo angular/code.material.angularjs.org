93ed27988b32fe65f529efc137bdf8ec93ff2df5
kbae00 <kbae@google.com>
2017-07-20 18:09:53 +0000

    snapshot: 1.1.4-master-bf6e567

--- HEAD/docs-demo-scripts.js
+++ HEAD/docs-demo-scripts.js
@@ -3120,6 +3120,21 @@ angular.module('toastDemo1', ['ngMaterial'])
 
 })();
 
+angular.module('tooltipDemo', ['ngMaterial'])
+    .controller('AppCtrl', AppCtrl);
+
+function AppCtrl($scope) {
+  $scope.demo = {
+    showTooltip: false,
+    tipDirection: 'bottom'
+  };
+
+  $scope.demo.delayTooltip = undefined;
+  $scope.$watch('demo.delayTooltip', function(val) {
+    $scope.demo.delayTooltip = parseInt(val, 10) || 0;
+  });
+}
+
 
 angular.module('toolbarDemo1', ['ngMaterial'])
 
@@ -3145,21 +3160,6 @@ angular.module('toolbarDemo2', ['ngMaterial'])
     });
   }
 });
-
-angular.module('tooltipDemo', ['ngMaterial'])
-    .controller('AppCtrl', AppCtrl);
-
-function AppCtrl($scope) {
-  $scope.demo = {
-    showTooltip: false,
-    tipDirection: 'bottom'
-  };
-
-  $scope.demo.delayTooltip = undefined;
-  $scope.$watch('demo.delayTooltip', function(val) {
-    $scope.demo.delayTooltip = parseInt(val, 10) || 0;
-  });
-}
 
 (function () {
   'use strict';
--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.5.5",
   "version": "1.1.4",
   "repository": "https://github.com/angular/material",
-  "date": "2017-07-18 12:51:26 -0700",
-  "commit": "2247248ab74df2b46095ccdcf20a7e6d924def21"
+  "date": "2017-07-20 10:59:24 -0700",
+  "commit": "bf6e5679535a8a6fb80d23d42d657be522ba038a"
 });
