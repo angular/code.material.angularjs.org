a8577d1a2fd58f5527c9a488428c9b4200fa1a10
Zack Goldberg <zsgoldberg@gmail.com>
2017-07-21 20:01:07 +0000

    snapshot: 1.1.4-master-b084f3d

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.5.5",
   "version": "1.1.4",
   "repository": "https://github.com/angular/material",
-  "date": "2017-07-20 10:59:24 -0700",
-  "commit": "bf6e5679535a8a6fb80d23d42d657be522ba038a"
+  "date": "2017-07-21 12:50:26 -0700",
+  "commit": "b084f3d01651904b9f29852af851e4d926fa57e3"
 });
