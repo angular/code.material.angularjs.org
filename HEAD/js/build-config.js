82d25507f72fc78bad93214c434e992855de75a4
Jeremy Elbourn <jelbourn@gmail.com>
2017-12-05 17:48:26 +0000

    snapshot: 1.1.5-master-c4dccdc

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.5.5",
   "version": "1.1.5",
   "repository": "https://github.com/angular/material",
-  "date": "2017-12-05 09:36:54 -0800",
-  "commit": "1956d2235d60944cc7f1ef84f282cfd1290ba12d"
+  "commit": "c4dccdc8eb79ed9d6852fc24e2545481b07d0c21",
+  "date": "2017-12-05 09:37:21 -0800"
 });
