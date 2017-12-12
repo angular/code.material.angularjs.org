d5d3cda1f7409f3d670f9fab39c20169dc9248aa
Elad Bezalel <Elad.bezalel@gmail.com>
2017-12-12 18:04:36 +0000

    snapshot: 1.1.5-master-bbb719d

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.5.5",
   "version": "1.1.5",
   "repository": "https://github.com/angular/material",
-  "date": "2017-12-12 09:47:59 -0800",
-  "commit": "7256670ae4862eb7091effee6f50dbc66bc3b42e"
+  "date": "2017-12-12 09:54:27 -0800",
+  "commit": "bbb719d73155dc22d95c891d26d1827edb5718dd"
 });
