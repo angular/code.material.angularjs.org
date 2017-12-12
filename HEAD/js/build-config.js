557725e852556d8accba06b0ad09c16141317a5e
Alejandro López <a.lopez.sanchez@outlook.es>
2017-12-12 17:59:03 +0000

    snapshot: 1.1.5-master-7256670

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.5.5",
   "version": "1.1.5",
   "repository": "https://github.com/angular/material",
-  "commit": "0bac4621fa1c5eead200d058e59938e8a51a384c",
-  "date": "2017-12-05 09:36:18 -0800"
+  "date": "2017-12-12 09:47:59 -0800",
+  "commit": "7256670ae4862eb7091effee6f50dbc66bc3b42e"
 });
