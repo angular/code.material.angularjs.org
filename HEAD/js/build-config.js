8b1c6f7f72a55bbd1a6e94b44f5e7a99933b26eb
Oliver Salzburg <oliver.salzburg@gmail.com>
2018-01-22 17:16:03 +0000

    snapshot: 1.1.6-master-27d0f7c

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.5.5",
   "version": "1.1.6",
   "repository": "https://github.com/angular/material",
-  "date": "2018-01-19 16:07:04 -0800",
-  "commit": "f8289dcde2689ab99fd31fdfdcd56a8d615eb3ff"
+  "commit": "27d0f7c84642832d95b26bfd509b3153cc273251",
+  "date": "2018-01-22 09:04:08 -0800"
 });
