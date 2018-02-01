4325455dcbb02c68ef287b5bc01c8a5c3befda95
Gilly Barr <gillyb@gmail.com>
2018-02-01 23:46:47 +0000

    snapshot: 1.1.6-master-827990e

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.6.7",
   "version": "1.1.6",
   "repository": "https://github.com/angular/material",
-  "date": "2018-01-29 18:21:55 -0800",
-  "commit": "15a841053d31650f6058ac7bbb0c3aef04cc4c0a"
+  "date": "2018-02-01 15:36:40 -0800",
+  "commit": "827990e31bdcd01afd064799cbc3263ad13fa340"
 });
