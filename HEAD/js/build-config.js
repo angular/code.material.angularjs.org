8322aa9b48e994292eda1a382d6d4d8a99582f60
Carlos Lopez <clshortfuse@gmail.com>
2018-02-09 23:55:47 +0000

    snapshot: 1.1.7-master-0f15e39

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.6.7",
   "version": "1.1.7",
   "repository": "https://github.com/angular/material",
-  "date": "2018-02-09 11:29:11 -0800",
-  "commit": "0d276f31bb551a49760b987dae02d95178982e17"
+  "commit": "0f15e3918d2d20b229615a98c8a59615720d168f",
+  "date": "2018-02-09 15:39:17 -0800"
 });
