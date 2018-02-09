14456db068c7d855cfe52132cae645738094ef08
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2018-02-09 19:40:33 +0000

    snapshot: 1.1.7-master-0d276f3

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.6.7",
   "version": "1.1.7",
   "repository": "https://github.com/angular/material",
-  "commit": "4d99e36f3ab177bb736de4b47c7e8dea8622a8df",
-  "date": "2018-02-08 13:21:50 -0800"
+  "date": "2018-02-09 11:29:11 -0800",
+  "commit": "0d276f31bb551a49760b987dae02d95178982e17"
 });
