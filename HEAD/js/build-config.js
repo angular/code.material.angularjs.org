e82cc825d3eba04e5fd6c44a7b8b68d797863c00
Ed Knowles <mail@eknowles.com>
2018-02-16 19:08:30 +0000

    snapshot: 1.1.7-master-69470a6

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.6.7",
   "version": "1.1.7",
   "repository": "https://github.com/angular/material",
-  "commit": "0f15e3918d2d20b229615a98c8a59615720d168f",
-  "date": "2018-02-09 15:39:17 -0800"
+  "date": "2018-02-16 10:58:02 -0800",
+  "commit": "69470a6dd42aae98b61868c0e9ef5da3c568811b"
 });
