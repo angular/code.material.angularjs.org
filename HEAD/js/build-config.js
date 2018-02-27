e26648d8339151e9ca195812e536e784131fe9c0
Jon Card <jon.card@gmail.com>
2018-02-27 18:32:25 +0000

    snapshot: 1.1.7-master-c5ec316

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.6.7",
   "version": "1.1.7",
   "repository": "https://github.com/angular/material",
-  "date": "2018-02-21 11:28:29 -0800",
-  "commit": "60e2393f915d808518c2cb10711a5fd6859b86e0"
+  "date": "2018-02-27 10:20:22 -0800",
+  "commit": "c5ec316b2218f59058cb8b28f9585e70ce41eecd"
 });
