55b023098bebc6e3f765c4422f49b5a22c858518
Michael Prentice <splaktar@gmail.com>
2018-03-07 19:39:47 +0000

    snapshot: 1.1.7-master-d3cb20d

--- HEAD/docs-demo-scripts.js
+++ HEAD/docs-demo-scripts.js
@@ -439,6 +439,18 @@ angular.module('bottomSheetDemo1', ['ngMaterial'])
   });
 
 
+angular.module('buttonsDemo1', ['ngMaterial'])
+
+.controller('AppCtrl', function($scope) {
+  $scope.title1 = 'Button';
+  $scope.title4 = 'Warn';
+  $scope.isDisabled = true;
+
+  $scope.googleUrl = 'http://google.com';
+
+});
+
+
 angular.module('cardDemo1', ['ngMaterial'])
 
 .controller('AppCtrl', function($scope) {
@@ -466,18 +478,6 @@ angular.module('cardDemo3', ['ngMaterial'])
 }])
 .controller('AppCtrl', function($scope) {
   $scope.imagePath = 'img/washedout.png';
-});
-
-
-angular.module('buttonsDemo1', ['ngMaterial'])
-
-.controller('AppCtrl', function($scope) {
-  $scope.title1 = 'Button';
-  $scope.title4 = 'Warn';
-  $scope.isDisabled = true;
-
-  $scope.googleUrl = 'http://google.com';
-
 });
 
 
--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.6.7",
   "version": "1.1.7",
   "repository": "https://github.com/angular/material",
-  "commit": "a4cc1869e63c6fb80e78a0c875329878cb3e34db",
-  "date": "2018-03-05 18:04:40 -0500"
+  "commit": "d3cb20db8c1c6cdfb4019e0c7d58b93d856beefd",
+  "date": "2018-03-07 11:29:25 -0800"
 });
