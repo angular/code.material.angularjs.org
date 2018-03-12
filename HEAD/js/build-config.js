4f5d7fd92c7e885927a00fdba18949d71f747c79
Tiberiu Zuld <tiberiuzuld@gmail.com>
2018-03-12 17:04:12 +0000

    snapshot: 1.1.7-master-2ef87f4

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.6.7",
   "version": "1.1.7",
   "repository": "https://github.com/angular/material",
-  "date": "2018-03-07 13:10:35 -0800",
-  "commit": "6e46351c8ae7e04dad9eccbe852f1af2a9c2ae6f"
+  "commit": "2ef87f45365c01ba26b2cf70f463919342f2a817",
+  "date": "2018-03-12 09:52:01 -0700"
 });
