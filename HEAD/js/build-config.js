cf19beee0a9fd3c38f815a9a4b28672932b98a63
Philippe MARTIN <contact@elol.fr>
2018-03-12 22:35:23 +0000

    snapshot: 1.1.7-master-9ced357

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.6.7",
   "version": "1.1.7",
   "repository": "https://github.com/angular/material",
-  "commit": "87b3c3b4f16b7c13024d9c61f7e7a9891d609c75",
-  "date": "2018-03-12 14:02:12 -0700"
+  "commit": "9ced35789163fb1df98eaba887d54f23d8b85f5b",
+  "date": "2018-03-12 15:24:58 -0700"
 });
