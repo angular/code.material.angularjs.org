1e8d7bcaa3c1de23682338c31a6833f19f40f2a8
Karan Sapolia <karansapolia@users.noreply.github.com>
2018-03-16 15:53:13 +0000

    snapshot: 1.1.7-master-52efe32

--- HEAD/docs-demo-scripts.js
+++ HEAD/docs-demo-scripts.js
@@ -1207,6 +1207,24 @@ angular.module('dividerDemo1', ['ngMaterial'])
 (function() {
   'use strict';
 
+  angular.module('fabSpeedDialDemoBasicUsage', ['ngMaterial'])
+    .controller('DemoCtrl', function() {
+      this.topDirections = ['left', 'up'];
+      this.bottomDirections = ['down', 'right'];
+
+      this.isOpen = false;
+
+      this.availableModes = ['md-fling', 'md-scale'];
+      this.selectedMode = 'md-fling';
+
+      this.availableDirections = ['up', 'down', 'left', 'right'];
+      this.selectedDirection = 'up';
+    });
+})();
+
+(function() {
+  'use strict';
+
   angular.module('fabSpeedDialDemoMoreOptions', ['ngMaterial'])
     .controller('DemoCtrl', function($scope, $mdDialog, $timeout) {
       var self = this;
@@ -1254,24 +1272,6 @@ angular.module('dividerDemo1', ['ngMaterial'])
           targetEvent: $event
         });
       };
-    });
-})();
-
-(function() {
-  'use strict';
-
-  angular.module('fabSpeedDialDemoBasicUsage', ['ngMaterial'])
-    .controller('DemoCtrl', function() {
-      this.topDirections = ['left', 'up'];
-      this.bottomDirections = ['down', 'right'];
-
-      this.isOpen = false;
-
-      this.availableModes = ['md-fling', 'md-scale'];
-      this.selectedMode = 'md-fling';
-
-      this.availableDirections = ['up', 'down', 'left', 'right'];
-      this.selectedDirection = 'up';
     });
 })();
 
--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.6.7",
   "version": "1.1.7",
   "repository": "https://github.com/angular/material",
-  "date": "2018-03-15 11:11:55 -0700",
-  "commit": "cec409a9ab3b5927f8b7726b160383a0d259c6fe"
+  "commit": "52efe32da76af6795497c7da138f37dc48fbaea7",
+  "date": "2018-03-16 08:42:15 -0700"
 });
