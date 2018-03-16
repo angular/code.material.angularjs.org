ac36893258e71665627cd6a4da4b1dbdacf7225a
Miles Malerba <mmalerba@google.com>
2018-03-16 21:04:13 +0000

    snapshot: 1.1.8-master-8e5159e

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -1,7 +1,7 @@
 angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.6.7",
-  "version": "1.1.7",
+  "version": "1.1.8",
   "repository": "https://github.com/angular/material",
-  "date": "2018-03-16 13:28:09 -0700",
-  "commit": "b4d36f33d7a426abbe10cfecdcafe1404ab5c1ee"
+  "commit": "8e5159e90635524af988dc866a79abdac755d10f",
+  "date": "2018-03-16 13:53:15 -0700"
 });
