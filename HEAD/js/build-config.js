64cb292ccff8783a063c57546cd2b7e7366f3295
Immanuel von Detten <i.vondetten@gmail.com>
2018-03-24 00:06:54 +0000

    snapshot: 1.1.8-master-1ef9ec3

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.6.7",
   "version": "1.1.8",
   "repository": "https://github.com/angular/material",
-  "commit": "c62282fecc028c1e2ae36be6b01a5ca6adca69fa",
-  "date": "2018-03-20 09:34:44 -0700"
+  "date": "2018-03-23 16:56:32 -0700",
+  "commit": "1ef9ec308d75493e329f81bb60173083075aaac5"
 });
