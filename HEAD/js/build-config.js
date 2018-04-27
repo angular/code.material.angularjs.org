568d551d5ef56cb6d8ae27c53f26a64643d91b76
Andrew Seguin <andrewjs@google.com>
2018-04-27 21:48:50 +0000

    snapshot: 1.1.9-master-e1b3407

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -1,7 +1,7 @@
 angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.6.7",
-  "version": "1.1.8",
+  "version": "1.1.9",
   "repository": "https://github.com/angular/material",
-  "date": "2018-04-27 13:38:59 -0700",
-  "commit": "27c0c810cb6336e7e61f16cfd8294a7dc527e707"
+  "commit": "e1b34076ab78f0644239d8017efe8baf505f8580",
+  "date": "2018-04-27 14:34:26 -0700"
 });
