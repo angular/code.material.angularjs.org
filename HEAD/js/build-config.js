4d8fa2fc88226bc2cfcfcec2c36c2515d9e46c00
rudzikdawid <rudzikdawid@gmail.com>
2018-05-16 22:39:11 +0000

    snapshot: 1.1.9-master-d607e17

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.6.7",
   "version": "1.1.9",
   "repository": "https://github.com/angular/material",
-  "date": "2018-05-04 14:21:51 -0700",
-  "commit": "db1a85da8fc870440a84d782c0465c869d1d825c"
+  "date": "2018-05-16 14:44:07 -0700",
+  "commit": "d607e17986e9169138ef168c25cc93f188fcb656"
 });
