c2c23d5755de57ab8cae2a2ad5bcf08320e612b7
Lucas Bersier <bers.lucas@gmail.com>
2018-06-22 15:37:46 +0000

    snapshot: 1.1.9-master-81eb46f

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.6.7",
   "version": "1.1.9",
   "repository": "https://github.com/angular/material",
-  "date": "2018-06-22 07:59:25 -0700",
-  "commit": "a6bca73d504d127e5a40cf38857fdcdaf4e15aaa"
+  "date": "2018-06-22 08:25:57 -0700",
+  "commit": "81eb46f1087661a5beafb1e0dbf2bc0826231791"
 });
