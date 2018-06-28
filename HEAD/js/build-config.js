9d87c1ec8b7a7a1138fa48b508e3879c505d207b
Joey Perrott <josephperrott@gmail.com>
2018-06-28 19:26:13 +0000

    snapshot: 1.1.10-master-3ef9a02

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -1,7 +1,7 @@
 angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.6.7",
-  "version": "1.1.9",
+  "version": "1.1.10",
   "repository": "https://github.com/angular/material",
-  "date": "2018-06-26 07:24:58 -0700",
-  "commit": "41c9d00dbb3cd85a31c3a02693854f25b2f7a8df"
+  "date": "2018-06-28 12:15:38 -0700",
+  "commit": "3ef9a02b56108142ab6f6c6be27828f364191a68"
 });
