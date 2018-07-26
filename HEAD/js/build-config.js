1989b77cf7bf7a47095f4b299c1b46bdcce30377
marosoft <mdabrowski84@gmail.com>
2018-07-26 17:49:31 +0000

    snapshot: 1.1.10-master-d48c5b8

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.6.7",
   "version": "1.1.10",
   "repository": "https://github.com/angular/material",
-  "commit": "afc226268fdd68eb62fdd595b5524ecb312ffbee",
-  "date": "2018-07-26 10:08:24 -0700"
+  "commit": "d48c5b85cd6925a84eb6c80f809ec967fab41fac",
+  "date": "2018-07-26 10:37:36 -0700"
 });
