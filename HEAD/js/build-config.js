623024e2736375fa26e5002889ac15e4cf959353
codymikol <codymikol@gmail.com>
2018-08-13 20:42:22 +0000

    snapshot: 1.1.10-master-1d73d81

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.6.7",
   "version": "1.1.10",
   "repository": "https://github.com/angular/material",
-  "date": "2018-08-09 07:41:35 -0700",
-  "commit": "e3c1a5ce447152f96be834bdc4aeeefc84e2a3ce"
+  "commit": "1d73d81ea2f0a1dd808e7623985f140ecb804bc6",
+  "date": "2018-08-13 13:29:41 -0700"
 });
