a9b54bd932afd3e8997645cb7dd307d2f07ca57e
Daniel Melchior <d.melchior@me.com>
2018-09-04 22:16:38 +0000

    snapshot: 1.1.10-master-f776bf7

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.7.2",
   "version": "1.1.10",
   "repository": "https://github.com/angular/material",
-  "commit": "8aaa37a8c6d18a6d962f0bde78b5429e4c92a571",
-  "date": "2018-09-04 15:04:40 -0700"
+  "commit": "f776bf72703f7a53f788d918a09cb126b76d05d7",
+  "date": "2018-09-04 15:05:01 -0700"
 });
