f526266c9cf2dc213c6f49181c52241b67c286b8
Benedikt Roßgardt <benedikt@rossgardt.de>
2018-09-04 22:31:46 +0000

    snapshot: 1.1.10-master-aa30ada

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.7.2",
   "version": "1.1.10",
   "repository": "https://github.com/angular/material",
-  "commit": "affe84b92fc957ccac0c92e09279c7768dceb9c9",
-  "date": "2018-09-04 15:18:44 -0700"
+  "date": "2018-09-04 15:19:32 -0700",
+  "commit": "aa30ada04bbd04c2e5fec99dd0b9357098d291db"
 });
