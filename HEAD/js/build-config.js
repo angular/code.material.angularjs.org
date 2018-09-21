d2a980c31f8e40877a4a12e16b0af851fb76c983
Michael Dokolin <dokmic@gmail.com>
2018-09-21 17:10:02 +0000

    snapshot: 1.1.10-master-dfba062

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.7.2",
   "version": "1.1.10",
   "repository": "https://github.com/angular/material",
-  "date": "2018-09-21 09:59:55 -0700",
-  "commit": "6596cc7bb8a731ca8f98d9e2b3f5194fea8779f3"
+  "date": "2018-09-21 10:00:12 -0700",
+  "commit": "dfba06266cd6eddad48d2ede28e8bba6755223b1"
 });
