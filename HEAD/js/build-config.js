64feb5d9481bcd03ef53cce5957c2dce0f271b94
Michael Prentice <splaktar@gmail.com>
2018-10-23 19:04:48 +0000

    snapshot: 1.1.10-master-841e8b2

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.7.4",
   "version": "1.1.10",
   "repository": "https://github.com/angular/material",
-  "commit": "f616b25693289ed9c2dbfdb298c635dce79c03ce",
-  "date": "2018-10-19 19:11:46 -0700"
+  "date": "2018-10-23 11:54:58 -0700",
+  "commit": "841e8b22349adfd31e74bbc3a41c2f0c7dc07d41"
 });
