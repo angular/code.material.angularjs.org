e8de564aab1df31abc3a75908ec330f43ddad214
Jagmeet Bhamber <jagmeetbhamber@hotmail.com>
2018-11-01 21:25:17 +0000

    snapshot: 1.1.10-master-4e8bef8

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.7.4",
   "version": "1.1.10",
   "repository": "https://github.com/angular/material",
-  "date": "2018-10-26 14:48:31 -0700",
-  "commit": "6b29548a8933cc8a378fda3b42d52c4a626d58db"
+  "commit": "4e8bef88497060f2c8048866daa0a6fa14a3e248",
+  "date": "2018-11-01 14:14:49 -0700"
 });
