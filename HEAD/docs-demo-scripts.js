ed0f6b9038813b0f6c8cf7e6761f00c3229eca78
Jeremy Elbourn <jelbourn@google.com>
2018-12-21 19:39:38 +0000

    snapshot: 1.1.11-master-54e3413

--- HEAD/docs-demo-scripts.js
+++ HEAD/docs-demo-scripts.js
@@ -2939,31 +2939,6 @@ angular
     }
   });
 
-angular.module('sliderDemo1', ['ngMaterial'])
-  .config(function ($mdIconProvider) {
-    $mdIconProvider.iconSet('device', 'img/icons/sets/device-icons.svg', 24);
-  })
-  .controller('AppCtrl', function ($scope) {
-
-    $scope.color = {
-      red: Math.floor(Math.random() * 255),
-      green: Math.floor(Math.random() * 255),
-      blue: Math.floor(Math.random() * 255)
-    };
-
-    $scope.rating1 = 3;
-    $scope.rating2 = 2;
-    $scope.rating3 = 4;
-
-    $scope.disabled1 = Math.floor(Math.random() * 100);
-    $scope.disabled2 = 0;
-    $scope.disabled3 = 70;
-
-    $scope.invert = Math.floor(Math.random() * 100);
-
-    $scope.isDisabled = true;
-  });
-
 
 angular.module('sliderDemo2', ['ngMaterial'])
 
@@ -3062,6 +3037,31 @@ angular.module('subheaderBasicDemo', ['ngMaterial'])
       },
     ];
 });
+
+angular.module('sliderDemo1', ['ngMaterial'])
+  .config(function ($mdIconProvider) {
+    $mdIconProvider.iconSet('device', 'img/icons/sets/device-icons.svg', 24);
+  })
+  .controller('AppCtrl', function ($scope) {
+
+    $scope.color = {
+      red: Math.floor(Math.random() * 255),
+      green: Math.floor(Math.random() * 255),
+      blue: Math.floor(Math.random() * 255)
+    };
+
+    $scope.rating1 = 3;
+    $scope.rating2 = 2;
+    $scope.rating3 = 4;
+
+    $scope.disabled1 = Math.floor(Math.random() * 100);
+    $scope.disabled2 = 0;
+    $scope.disabled3 = 70;
+
+    $scope.invert = Math.floor(Math.random() * 100);
+
+    $scope.isDisabled = true;
+  });
 
 angular.module('demoSwipe', ['ngMaterial'])
   .controller('demoSwipeCtrl', function($scope, $log) {
--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -1,7 +1,7 @@
 angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.7.5",
-  "version": "1.1.10",
+  "version": "1.1.11",
   "repository": "https://github.com/angular/material",
-  "commit": "47527f245acc8382a69a42a07989fd5f646e9744",
-  "date": "2018-12-20 13:26:53 -0800"
+  "commit": "54e3413a2b0afac432a474bc82e801f425cc6269",
+  "date": "2018-12-21 11:31:03 -0800"
 });
