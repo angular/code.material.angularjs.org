9c95726c8b9265e580fe2befa1f9a61996f8919b
Miles Malerba <mmalerba@google.com>
2019-03-18 10:14:19 -0700

    release: version 1.1.14

---
+++ 1.1.14/demo-partials/button/demoBasicUsage/script.js
@@ -0,0 +1,7 @@
+angular.module('buttonsDemoBasic', ['ngMaterial'])
+.controller('AppCtrl', function($scope) {
+  $scope.title1 = 'Button';
+  $scope.title4 = 'Warn';
+  $scope.isDisabled = true;
+  $scope.googleUrl = 'http://google.com';
+});
