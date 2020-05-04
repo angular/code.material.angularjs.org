5ef931a1c4fdf126297d6cf4dbaea8df8342301b
Michael Prentice <splaktar@gmail.com>
2020-05-04 23:34:27 +0000

    snapshot: v1.1.22-master-74dd2cd

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.7.9",
   "version": "1.1.22",
   "repository": "https://github.com/angular/material",
-  "commit": "b486a4104e2fdfdac8563b0c301fb3bfaf060142",
-  "date": "2020-05-01 16:10:53 -0400"
+  "commit": "74dd2cdb3da74e19cf44d3ce1428e3f3dd65b281",
+  "date": "2020-05-04 19:28:01 -0400"
 });
