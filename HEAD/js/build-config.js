56c728d4f1d701f7103fe810bcf3b0f8eed65b71
KyleKatarn <kylekatarnls@gmail.com>
2020-12-09 13:30:26 +0000

    snapshot: v1.2.1-master-e6b5482

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ angular.module('docsApp').constant('BUILDCONFIG', {
   "ngVersion": "1.8.2",
   "version": "1.2.1",
   "repository": "https://github.com/angular/material",
-  "commit": "71dc4eb38d8120ba1e49a9420debfe7aaf0c8e34",
-  "date": "2020-12-09 08:23:34 -0500"
+  "commit": "e6b5482231520a69ab196a2e508f1c1dc1f3a081",
+  "date": "2020-12-09 08:25:21 -0500"
 });
